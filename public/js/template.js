d15c346633ada3bf70b914611278edc5f049fe42
Steven Chow <steven.chow@live.ca>
2022-05-04 09:30:39 -0700

    Add code to link header, footer, navigation templates to pages.

---
+++ public/js/template.js
@@ -0,0 +1,11 @@
+function loadSkeleton() {
+    $("#navigation").load("./template/navbar.html")
+    $("#header").load("./template/header.html")
+    $("#footer").load("./template/footer.html")
+}
+
+function setup() {
+    loadSkeleton()
+}
+
+$(document).ready(setup)

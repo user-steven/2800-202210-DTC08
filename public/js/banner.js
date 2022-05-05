14bf850bc676c1f907d597b77249d8af17c9e792
Alexander Farlinger <alexanderfarlinger@gmail.com>
2022-05-05 15:08:34 -0700

    Credit Commit
    credited w3schools for providing a base for the banner code I used
        @author contribute@w3schools.com
        @see https://www.w3schools.com/howto/howto_js_slideshow.asp

--- public/js/banner.js
+++ public/js/banner.js
@@ -1,3 +1,7 @@
+//     I found this code on https://www.w3schools.com/
+//     @author contribute@w3schools.com
+//     @see https://www.w3schools.com/howto/howto_js_slideshow.asp
+
 let slideIndex = 2;
 showSlides(slideIndex);
 

cf22b4c63e9e64d3f39fde48f97b03e114f4d344
Neil Cao <caofangxin7@gmail.com>
2022-05-05 13:11:31 -0700

    added session demo

--- server.js
+++ server.js
@@ -1,8 +1,39 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.listen(5100, function(err){
-    if(err) console.log(err);
-    })
+app.use(express.static("./public"));
 
-app.use(express.static("./public"))
+var session = require("express-session");
+
+app.use(session({ secret: "ssshhhhh", saveUninitialized: true, resave: true }));
+
+users = {
+  Neil: "123456",
+  Nick: "456789",
+};
+
+app.listen(5100, function (err) {
+  if (err) console.log(err);
+});
+
+app.get("/", function (req, res) {
+  if (req.session.authenticated) res.send(`Hi ${req.session.user} !`);
+  else {
+    res.redirect("/login");
+  }
+});
+
+app.get("/login/", function (req, res, next) {
+  res.send("Please provide the credentials through the URL");
+});
+
+app.get("/login/:user/:pass", function (req, res, next) {
+  if (users[req.params.user] == req.params.pass) {
+    req.session.authenticated = true;
+    req.session.user = req.params.user;
+    res.send("Successful Login!");
+  } else {
+    req.session.authenticated = false;
+    res.send("Failed Login!");
+  }
+});

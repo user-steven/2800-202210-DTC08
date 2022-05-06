c3e5787341a3e75e9cee9a75ee834cb7765f91ae
Neil Cao <caofangxin7@gmail.com>
2022-05-05 22:39:25 -0700

    add contact page

--- server.js
+++ server.js
@@ -1,79 +1,82 @@
-const express = require('express');
-const app = express()
+const express = require("express");
+const app = express();
 
-const session = require("express-session")
+const session = require("express-session");
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
 const bodyparser = require("body-parser");
-app.use(bodyparser.urlencoded({
-  extended: true
-}))
+app.use(
+  bodyparser.urlencoded({
+    extended: true,
+  })
+);
 
-app.use(session({
+app.use(
+  session({
     secret: "dtc8",
     saveUnintialized: true,
-    resave: true
-}))
+    resave: true,
+  })
+);
 
-app.listen(5100, function(err){
-    if(err) console.log(err);
-    })
+app.listen(5100, function (err) {
+  if (err) console.log(err);
+});
 
 const users = {
-    "superUser@dtc8.ca" : "super",
-    "testUser@dtc8.ca" : "test"
-}
+  "superUser@dtc8.ca": "super",
+  "testUser@dtc8.ca": "test",
+};
 
-var user_data = [{email: 'admin@bcit.ca', password: 'bcit', admin: true}]
+var user_data = [{ email: "admin@bcit.ca", password: "bcit", admin: true }];
 
-app.use(express.static("./public"))
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-    res.render(__dirname+ "/public/index.ejs")
-})
+  res.render(__dirname + "/public/index.ejs");
+});
 
 app.post("/", (req, res) => {
-    if (req.body.logOut) {
-        req.session.authenticated = false
-        req.session.user = undefined
-        res.render(__dirname+ "/public/index.ejs")
-    } else if (users[req.body.loginEmail] == req.body.loginPass) {
-        req.session.authenticated = true;
-        req.session.user = req.body.loginEmail
-        console.log("login sucessful");
-        res.render(__dirname+ "/public/index.ejs")
-    } else {
-        console.log("wrong credentials");
-        res.redirect("/login")
-    }
-})
+  if (req.body.logOut) {
+    req.session.authenticated = false;
+    req.session.user = undefined;
+    res.render(__dirname + "/public/index.ejs");
+  } else if (users[req.body.loginEmail] == req.body.loginPass) {
+    req.session.authenticated = true;
+    req.session.user = req.body.loginEmail;
+    console.log("login sucessful");
+    res.render(__dirname + "/public/index.ejs");
+  } else {
+    console.log("wrong credentials");
+    res.redirect("/login");
+  }
+});
 
 app.get("/login", (req, res) => {
-    if (req.session.authenticated) {
-        res.redirect("/")
-        console.log("already have a session");
-    } else {
-        res.render(__dirname + "/public/login.ejs")
-    } 
-})
+  if (req.session.authenticated) {
+    res.redirect("/");
+    console.log("already have a session");
+  } else {
+    res.render(__dirname + "/public/login.ejs");
+  }
+});
 
 app.get("/signup", (req, res) => {
-    if (req.session.authenticated) {
-        res.redirect("/")
-        console.log("already signed up");
-    } else {
-        res.render(__dirname + "/public/registration.ejs")
-    }
-})
-
+  if (req.session.authenticated) {
+    res.redirect("/");
+    console.log("already signed up");
+  } else {
+    res.render(__dirname + "/public/registration.ejs");
+  }
+});
 
 app.post("/create_user", function (req, res) {
-    registerInfo = req.body
-    registerInfo["admin"] = false
-    user_data.push(registerInfo)
-    // console.log(registerInfo)
-    // console.log(user_data)
-    console.log("Registered")
-    return res.redirect("/login")
-})
+  registerInfo = req.body;
+  registerInfo["admin"] = false;
+  user_data.push(registerInfo);
+  // console.log(registerInfo)
+  // console.log(user_data)
+  console.log("Registered");
+  return res.redirect("/login");
+});

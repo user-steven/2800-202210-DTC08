bdc41941c73bc0e1b4a5b7d9b3f0e1a47ce82789
Alexander Farlinger <alexanderfarlinger@gmail.com>
2022-05-10 15:49:30 -0700

    CSS complete
    CSS styling for flexbox complete for dropdown menu, added minor javascript functionality. No links work yet

---
+++ public/js/header.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    var x = document.getElementById("dropdownMenu");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+}

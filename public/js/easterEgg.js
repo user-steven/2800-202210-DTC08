dcb982010fe3587ebc6fdba0ac5c7c76f52a23c4
Steven Chow <steven.chow@live.ca>
2022-05-16 18:34:09 -0700

    Integrate easter egg to donation page

--- public/js/easterEgg.js
+++ public/js/easterEgg.js
@@ -1,7 +1,10 @@
+var amountDonated = 0
+
 var confettiElement = document.getElementById('donationConfetti')
+
 var confettiSettings = {
     target: confettiElement,
-    max: 100,
+    max: amountDonated,
     size: 2,
     animate: true,
     props: ["circle", "square", "triangle", "line"],
@@ -15,4 +18,20 @@ var confettiSettings = {
 }
 var confetti = new ConfettiGenerator(confettiSettings)
 
-// confetti.render();
+function displayConfetti() {
+    amountDonated = parseInt(document.getElementById('amountDonated').value)
+    console.log(amountDonated)
+    if (amountDonated >= 50) {
+        confetti.render()
+        setTimeout(() => {
+            confetti.clear()
+        }, 5000)
+    }
+}
+
+function setup() {
+    document.getElementById('submitButton').addEventListener("click", displayConfetti)
+}
+
+
+$(document).ready(setup)

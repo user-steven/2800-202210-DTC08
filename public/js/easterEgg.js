ad6bc23970aa21a9abe4b08ccaf6ac6b817b790d
Steven Chow <steven.chow@live.ca>
2022-05-16 17:48:22 -0700

    Complete version 1 of confetti

--- public/js/easterEgg.js
+++ public/js/easterEgg.js
@@ -1,5 +1,18 @@
+var confettiElement = document.getElementById('donationConfetti')
+var confettiSettings = {
+    target: confettiElement,
+    max: 100,
+    size: 2,
+    animate: true,
+    props: ["circle", "square", "triangle", "line"],
+    colors: [[165, 104, 246], [230, 61, 135], [0, 199, 228], [253, 214, 126]],
+    clock: 25,
+    rotate: true,
+    width: screen.width,
+    height: screen.height,
+    start_from_edge: false,
+    respawn: true
+}
+var confetti = new ConfettiGenerator(confettiSettings)
 
-
-var confettiSettings = { target: 'my-canvas' };
-var confetti = new ConfettiGenerator(confettiSettings);
-confetti.render();
+// confetti.render();

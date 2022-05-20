bc2ac3b6ddc7670a54e1b3d86f9761c902dffe2a
Neil Cao <caofangxin7@gmail.com>
2022-05-19 18:34:58 -0700

    added css for the conflict profile page

--- public/js/conflictProfile.js
+++ public/js/conflictProfile.js
@@ -1,114 +1,112 @@
 var to_add;
-var articlesId
+var articlesId;
 
 async function authorize() {
-    var authorized;
-    await $.ajax({
-        type: `GET`,
-        url: `http://localhost:5100/authorization`,
-        success: function (response) {
-            authorized = response;
-            return;
-        }
-    })
-    return authorized;
+  var authorized;
+  await $.ajax({
+    type: `GET`,
+    url: `http://localhost:5100/authorization`,
+    success: function (response) {
+      authorized = response;
+      return;
+    },
+  });
+  return authorized;
 }
 
 async function checkDuplicates(id, route) {
-    var authorized = true;
-    await $.ajax({
-        type: `GET`,
-        url: `http://localhost:5100/${route}`,
-        success: function (data) {
-            data.filter(function (currentValue) {
-                if (currentValue == id) {
-                    authorized = false;
-                    return;
-                }
-            })
+  var authorized = true;
+  await $.ajax({
+    type: `GET`,
+    url: `http://localhost:5100/${route}`,
+    success: function (data) {
+      data.filter(function (currentValue) {
+        if (currentValue == id) {
+          authorized = false;
+          return;
         }
-    })
-    return authorized
+      });
+    },
+  });
+  return authorized;
 }
 
 async function watchConflict(id) {
+  if (!(await authorize())) {
+    window.alert("Log in first");
+    return;
+  }
 
-    if (!(await authorize())) {
-        window.alert("Log in first");
-        return;
-    }
-
-    if (!(await checkDuplicates(id, "getSavedConflicts"))) {
-        await $.ajax({
-            type: `POST`,
-            url: `http://localhost:5100/removeConflict/${id}`,
-            success: function (message) {
-                window.alert(message);
-            }
-        })
-        return;
-    }
-
+  if (!(await checkDuplicates(id, "getSavedConflicts"))) {
     await $.ajax({
-        type: `POST`,
-        url: `http://localhost:5100/saveConflict/${id}`,
-        success: function (message) {
-            window.alert(message);
-        }
-    })
+      type: `POST`,
+      url: `http://localhost:5100/removeConflict/${id}`,
+      success: function (message) {
+        window.alert(message);
+      },
+    });
+    return;
+  }
+
+  await $.ajax({
+    type: `POST`,
+    url: `http://localhost:5100/saveConflict/${id}`,
+    success: function (message) {
+      window.alert(message);
+    },
+  });
 }
 
 async function addToReadLater(id) {
+  if (!(await authorize())) {
+    window.alert("Log in first");
+    return;
+  }
 
-    if (!(await authorize())) {
-        window.alert("Log in first");
-        return;
-    }
+  if (!(await checkDuplicates(id, "getSavedNews"))) {
+    window.alert("Article already saved to read later list.");
+    return;
+  }
 
-    if (!(await checkDuplicates(id, "getSavedNews"))) {
-        window.alert("Article already saved to read later list.");
-        return;
-    }
-
-    await $.ajax({
-        type: `POST`,
-        url: `http://localhost:5100/saveArticle/${id}`,
-        success: function (message) {
-            window.alert(message);
-        }
-    })
+  await $.ajax({
+    type: `POST`,
+    url: `http://localhost:5100/saveArticle/${id}`,
+    success: function (message) {
+      window.alert(message);
+    },
+  });
 }
 
 async function showCards(data) {
-    to_add += `<div class="relatedNewsContainer">`
-    for(let i = 0; i < data.length; i++) {
-        await $.ajax({
-            type: `GET`,
-            url: `http://localhost:5100/getArticle/${data[i]}`,
-            success: function (article) {
-                to_add += 
-                `<div class="container">
+  to_add += `<div class="relatedNewsContainer">`;
+  for (let i = 0; i < data.length; i++) {
+    await $.ajax({
+      type: `GET`,
+      url: `http://localhost:5100/getArticle/${data[i]}`,
+      success: function (article) {
+        to_add += `<div class="container">
                     <h4><a href="${article[0].articleLink}"><b>${article[0].name}</b></a></h4>
                     <p><i>${article[0].publisher}</i></p>
                     <p>${article[0].date}</p>
-                    <button onclick="addToReadLater('${article[0]._id}');">Read Later</button>
-                 </div>`
-            }
-        })
-    }
-    to_add += `</div>`
-    jQuery("main").html(to_add)
+                    <br>
+                    <button class="later" onclick="addToReadLater('${article[0]._id}');">Read Later</button>
+                 </div>`;
+      },
+    });
+  }
+  to_add += `</div>`;
+  jQuery("main").html(to_add);
 }
 
-async function setup () {
-    to_add = ``;
-    articlesId = document.getElementsByTagName("BODY")[0].id;
+async function setup() {
+  to_add = ``;
+  articlesId = document.getElementsByTagName("BODY")[0].id;
 
-    await $.ajax({
-        type: `GET`,
-        url: `http://localhost:5100/getArticles/${articlesId}`,
-        success: showCards
-    })
+  await $.ajax({
+    type: `GET`,
+    url: `http://localhost:5100/getArticles/${articlesId}`,
+    success: showCards,
+  });
 }
 
-$(document).ready(setup)
+$(document).ready(setup);

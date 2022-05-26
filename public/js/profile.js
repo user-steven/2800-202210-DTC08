51b3af6bc52a4936de7080ff52816fa0b34932cc
Neil Cao <caofangxin7@gmail.com>
2022-05-25 18:47:26 -0700

    added remove button css

--- public/js/profile.js
+++ public/js/profile.js
@@ -17,7 +17,7 @@ async function showNews(data) {
   for (let i = 0; i < data.length; i++) {
     to_add += `<div class="container">
             <h4><a href="${data[i].articleLink}"><b>${data[i].name}</b></a></h4>
-            <button id="${data[i]._id}" onclick="removeNews(this);">Remove</button>
+            <button class="removeNews" id="${data[i]._id}" onclick="removeNews(this);">Remove</button>
          </div>`;
     jQuery(`#${data[i]._id}`).html(to_add);
   }
